163cba311cfc080d96e511238b094af388b038af
Ives Chiba <ivesjundi@gmail.com>
2024-05-15 17:29:41 -0300

    refactor :recycle: avoid duplicate methods and dbconfig

--- src/main/java/org/salesforce/repositories/ClienteRepository.java
+++ src/main/java/org/salesforce/repositories/ClienteRepository.java
@@ -1,5 +1,6 @@
 package org.salesforce.repositories;
 
+import org.salesforce.infrastructure.OracleDbConfiguration;
 import org.salesforce.models.Cliente;
 
 import java.sql.*;
@@ -10,31 +11,24 @@ import java.util.List;
 
 public class ClienteRepository {
 
-    public static final String URL_CONNECTION = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    public static final String USER = "rm553243";
-    public static final String PASSWORD = "180600";
+    public OracleDbConfiguration dbConfig;
 
     public ClienteRepository() {
+        dbConfig = new OracleDbConfiguration();
     }
 
     public List<Cliente> getClientes() {
 
         List<Cliente> lista = new ArrayList<>();
 
-        try (Connection connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+        try (
+            Connection connection = dbConfig.getConnection();
              PreparedStatement st = connection.prepareStatement("SELECT * FROM CLIENTE ");
              ResultSet rs = st.executeQuery()) {
 
             while (rs.next()) {
                 Cliente cliente = new Cliente();
-                cliente.setId(rs.getInt("clie_id"));
-                cliente.setNome(rs.getString("clie_nome"));
-                cliente.setSobrenome(rs.getString("clie_sobrenome"));
-                cliente.setEmail(rs.getString("clie_email"));
-                cliente.setTipo(rs.getString("clie_tipo"));
-                cliente.setIdioma(rs.getString("clie_idioma"));
-                cliente.setPais(rs.getString("clie_pais"));
-                cliente.setTelefone(rs.getString("clie_telefone"));
+                mapResultSetToCliente(rs, cliente);
                 lista.add(cliente);
 
             }
@@ -47,7 +41,8 @@ public class ClienteRepository {
     public Cliente getClienteById(int id) {
         Cliente cliente = null;
 
-        try (Connection connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+        try (
+            Connection connection = dbConfig.getConnection();
              PreparedStatement st = connection.prepareStatement("SELECT * FROM CLIENTE WHERE clie_id = ?")) {
 
             st.setInt(1, id);
@@ -55,14 +50,7 @@ public class ClienteRepository {
 
             if (rs.next()) {
                 cliente = new Cliente();
-                cliente.setId(rs.getInt("clie_id"));
-                cliente.setNome(rs.getString("clie_nome"));
-                cliente.setSobrenome(rs.getString("clie_sobrenome"));
-                cliente.setEmail(rs.getString("clie_email"));
-                cliente.setTipo(rs.getString("clie_tipo"));
-                cliente.setIdioma(rs.getString("clie_idioma"));
-                cliente.setPais(rs.getString("clie_pais"));
-                cliente.setTelefone(rs.getString("clie_telefone"));
+                mapResultSetToCliente(rs, cliente);
 
             }
         } catch (SQLException e) {
@@ -72,20 +60,15 @@ public class ClienteRepository {
     }
 
     public int createCliente(Cliente cliente) {
-        try (Connection connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+        try (
+            Connection connection = dbConfig.getConnection();
              PreparedStatement st = connection.prepareStatement("INSERT INTO CLIENTE (" +
-                     "clie_nome, clie_sobrenome, clie_email, clie_tipo, clie_idioma, clie_pais, " +
+                         "clie_nome, clie_sobrenome, clie_email, clie_tipo, clie_idioma, clie_pais, " +
                      "clie_telefone)" +
                      " VALUES " +
                      "(?, ?, ?, ?, ?, ?, ?)")) {
 
-            st.setString(1, cliente.getNome());
-            st.setString(2, cliente.getSobrenome());
-            st.setString(3, cliente.getEmail());
-            st.setString(4, cliente.getTipo());
-            st.setString(5, cliente.getIdioma());
-            st.setString(6, cliente.getPais());
-            st.setString(7, cliente.getTelefone());
+            prepareStatementForClienteInsert(cliente, st);
 
             return st.executeUpdate();
 
@@ -95,21 +78,18 @@ public class ClienteRepository {
         return 0;
     }
 
+
+
     public void updateCliente(Cliente cliente) {
-        try (Connection connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+        try (
+            Connection connection = dbConfig.getConnection();
              PreparedStatement st = connection.prepareStatement(
                      "UPDATE CLIENTE " +
                              "SET clie_nome = ?, clie_sobrenome = ?, clie_email = ?, clie_tipo = ?, " +
                              "clie_idioma = ?, clie_pais = ?, clie_telefone = ? " +
                              "WHERE clie_id = ?")) {
 
-            st.setString(1, cliente.getNome());
-            st.setString(2, cliente.getSobrenome());
-            st.setString(3, cliente.getEmail());
-            st.setString(4, cliente.getTipo());
-            st.setString(5, cliente.getIdioma());
-            st.setString(6, cliente.getPais());
-            st.setString(7, cliente.getTelefone());
+            prepareStatementForClienteInsert(cliente, st);
             st.setInt(8, cliente.getId());
 
             st.executeUpdate();
@@ -121,7 +101,8 @@ public class ClienteRepository {
 
     public void deleteById(int id) {
 
-        try (Connection connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+        try (
+            Connection connection = dbConfig.getConnection();
              PreparedStatement st = connection.prepareStatement(
                      "DELETE FROM CLIENTE WHERE clie_id = ?")) {
 
@@ -132,6 +113,27 @@ public class ClienteRepository {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private void mapResultSetToCliente(ResultSet rs, Cliente cliente) throws SQLException {
+        cliente.setId(rs.getInt("clie_id"));
+        cliente.setNome(rs.getString("clie_nome"));
+        cliente.setSobrenome(rs.getString("clie_sobrenome"));
+        cliente.setEmail(rs.getString("clie_email"));
+        cliente.setTipo(rs.getString("clie_tipo"));
+        cliente.setIdioma(rs.getString("clie_idioma"));
+        cliente.setPais(rs.getString("clie_pais"));
+        cliente.setTelefone(rs.getString("clie_telefone"));
+    }
+
+    private void prepareStatementForClienteInsert(Cliente cliente, PreparedStatement st) throws SQLException {
+        st.setString(1, cliente.getNome());
+        st.setString(2, cliente.getSobrenome());
+        st.setString(3, cliente.getEmail());
+        st.setString(4, cliente.getTipo());
+        st.setString(5, cliente.getIdioma());
+        st.setString(6, cliente.getPais());
+        st.setString(7, cliente.getTelefone());
     }
 
 }
